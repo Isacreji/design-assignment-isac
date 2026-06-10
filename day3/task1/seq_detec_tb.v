c282fd8c73daf7481d8303a0f840a00c49a347e5
Isac M Reji <167743529+Isacreji@users.noreply.github.com>
2026-06-10 14:56:41 +0530

    Update seq_detec_tb.v

--- day3/task1/seq_detec_tb.v
+++ day3/task1/seq_detec_tb.v
@@ -1,1 +1,49 @@
-
+module seq_detect_1110_tb();
+    reg clk_tb, rst_tb, din_tb;
+    wire detected_tb;
+    
+    seq_detect_1110 dut(clk_tb, rst_tb, din_tb, detected_tb);
+    
+    initial begin
+        clk_tb = 0;
+        rst_tb = 1;
+        din_tb = 0;
+    end
+    
+    always #5 clk_tb = ~clk_tb;
+    
+    initial begin
+        #10;
+        rst_tb = 0;
+        
+        #10;
+        din_tb = 1;
+        
+        #10;
+        din_tb = 1;
+        
+        #10;
+        din_tb = 1;
+        
+        #10;
+        din_tb = 0; 
+        
+        #10;
+        din_tb = 1;
+        
+        #10;
+        din_tb = 1; 
+        
+        #10;
+        din_tb = 1;
+        
+        #10;
+        din_tb = 1;
+        
+        #10;
+        din_tb = 0;
+        
+        #20;
+        $finish;
+    end
+endmodule
