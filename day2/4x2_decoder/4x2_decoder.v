a8385f7f9f156d6c0272d526acf29351396060d8
Isac M Reji <167743529+Isacreji@users.noreply.github.com>
2026-06-11 09:37:52 +0530

    Update 4x2_decoder.v

--- day2/4x2_decoder/4x2_decoder.v
+++ day2/4x2_decoder/4x2_decoder.v
@@ -1,40 +1,15 @@
-module usr (
-    input Clk,
-    input rst,
-    input Sin,
-    input [3:0] Pin,
-    input shift,
-    input load,
-    input [1:0] mod,
-    output Sout,
-    output [3:0] Pout
-);
-
-    reg [3:0] q;
-
-    assign Pout = q;
-    assign Sout = q[0];
-
-    always @(posedge Clk or posedge rst) begin
-        if (rst) begin
-            q <= 4'b0000;
-        end else begin
-            case (mod)
-                2'b00: begin
-                    if (shift) q <= {Sin, q[3:1]};
-                end
-                2'b01: begin
-                    if (shift) q <= {Sin, q[3:1]};
-                end
-                2'b10: begin
-                    if (load)       q <= Pin;
-                    else if (shift) q <= {1'b0, q[3:1]};
-                end
-                2'b11: begin
-                    if (load) q <= Pin;
-                end
-            endcase
-        end
-    end
-
+module encoder4by2(input[3:0]d,output reg [1:0]b);
+always @(*)
+begin
+if (d==4'b0001)
+b=2'b00;
+else if(d==4'b0010)
+b=2'b01;
+else if(d==4'b0100)
+b=2'b10;
+else if(d==4'b1000)
+b=2'b11;
+else
+b=2'bXX;
+end
 endmodule
