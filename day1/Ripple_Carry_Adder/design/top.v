0e22db3c6939eea89078d831e171c9e4676b5be1
Isac M Reji <167743529+Isacreji@users.noreply.github.com>
2026-06-11 09:36:17 +0530

    Update top.v

--- day1/Ripple_Carry_Adder/design/top.v
+++ day1/Ripple_Carry_Adder/design/top.v
@@ -1,31 +1,14 @@
-module tb_rca_4bit;
-
-    reg [3:0] a;
-    reg [3:0] b;
-    reg cin;
-
-    wire [3:0] sum;
-    wire cout;
-
-    rca_4bit uut (
-        .a(a), 
-        .b(b), 
-        .cin(cin), 
-        .sum(sum), 
-        .cout(cout)
-    );
-
-    initial begin
-        $monitor("Time=%0t | a=%b b=%b cin=%b | sum=%b cout=%b", $time, a, b, cin, sum, cout);
-
-        a = 4'b0000; b = 4'b0000; cin = 1'b0;
-        
-        #10 a = 4'b0101; b = 4'b0011; cin = 1'b0;
-        #10 a = 4'b1111; b = 4'b0001; cin = 1'b0;
-        #10 a = 4'b1010; b = 4'b0101; cin = 1'b1;
-        #10 a = 4'b1111; b = 4'b1111; cin = 1'b1;
-        
-        #10 $finish;
-    end
-      
+module rca_4bit(
+    input [3:0] a,
+    input [3:0] b,
+    input cin,
+    output [3:0] sum,
+    output cout)
+    ;
+    wire w1,w2,w3;
+    full_adder fa0 (.a(a[0]), .b(b[0]), .cin(cin), .sum(sum[0]), .cout(c1));
+    full_adder fa1 (.a(a[1]), .b(b[1]), .cin(c1),  .sum(sum[1]), .cout(c2));
+    full_adder fa2 (.a(a[2]), .b(b[2]), .cin(c2),  .sum(sum[2]), .cout(c3));
+    full_adder fa3 (.a(a[3]), .b(b[3]), .cin(c3),  .sum(sum[3]), .cout(cout));
+    
 endmodule
