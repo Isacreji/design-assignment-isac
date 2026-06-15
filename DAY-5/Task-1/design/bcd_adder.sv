c4fd7e7a424c245ed3c477b123a62fc2227afa8d
Isac M Reji <167743529+Isacreji@users.noreply.github.com>
2026-06-15 21:45:33 +0530

    Update bcd_adder.sv

--- DAY-5/Task-1/design/bcd_adder.sv
+++ DAY-5/Task-1/design/bcd_adder.sv
@@ -1,1 +1,20 @@
-
+module bcd_adder(
+input logic [3:0] a,
+  input logic [3:0] b,
+  input logic       cin,
+  output logic [3:0] sum,
+  output logic       cout
+);
+  logic [4:0] temp;
+  assign temp = a + b + cin;
+  
+  always_comb begin
+    if (temp > 9) begin
+      sum = temp + 6;
+      cout = 1;
+    end else begin
+      sum = temp;
+      cout = 0;
+    end
+  end
+endmodule
