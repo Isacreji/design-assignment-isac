1f2853f71458f5c7a787c7ba8798af4e01d08728
Isac M Reji <167743529+Isacreji@users.noreply.github.com>
2026-06-17 22:10:33 +0530

    Create transaction.sv

---
+++ DAY-8/Verification/transaction.sv
@@ -0,0 +1,17 @@
+class transaction;
+
+rand bit rw;
+rand bit [7:0] addr;
+rand bit [31:0] data;
+
+constraint c_addr
+{
+ addr inside {8'h00,8'h04,8'h08,8'h0C};
+}
+
+function void display(string tag);
+ $display("[%s] rw=%0d addr=%h data=%h",
+           tag,rw,addr,data);
+endfunction
+
+endclass
